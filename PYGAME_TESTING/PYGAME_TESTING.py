305a7bb6e41368611c863c4f3e9e766a2db501d1
FAdy200 <abuyehia200@gmail.com>
2020-12-22 21:39:35 +0200

    first start

--- PYGAME_TESTING/PYGAME_TESTING.py
+++ PYGAME_TESTING/PYGAME_TESTING.py
@@ -1,7 +1,26 @@
 import pygame
+import sys
 
 pygame.init()
 size = 840, 640
-
+speed = [0, 1]
 screen = pygame.display.set_mode(size)
 
+ball = pygame.image.load("intro_ball.gif")
+
+ballrect = ball.get_rect()
+
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT: sys.exit()
+#     ballrect = ballrect.move(speed)
+#     if ballrect.left < 0 or ballrect.right > size[0]:
+#         speed[0] = -speed[0]
+#     if ballrect.top < 0 or ballrect.bottom > size[1]:
+#         speed[1] = -speed[1]
+
+#     screen.blit(ball, ballrect)
+while True:
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(ball,color=(0,0,0),center=(2,2),radius=20)
+    pygame.display.flip()
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import pygame
+import sys
+from PIL import Image
+# image = Image.open("Webp.net-resizeimage (15).png")
+# mode = image.mode
+# size = image.size
+# data = image.tobytes()
+#
+# py_image = pygame.image.fromstring(data, size, mode)
+image = pygame.image.load("Webp.net-resizeimage (15).png")
+w, h = image.get_size()
+image2 = pygame.image.load("Untitled.png")
+sc = pygame.display.set_mode((w, h))
+mouse = pygame.mouse.get_pos()
+i = 0
+image3 = pygame.transform.rotate(image2, i)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # image = image.rotate(10)
+            i += 1
+            image3 = pygame.transform.rotate(image, i)
+            print(mouse)
+    mouse = pygame.mouse.get_pos()
+    # py_image = pygame.image.fromstring(data, size, mode)
+    rect = image3.get_rect()
+    rect.center = w/2, h/2
+    sc.fill((245, 245, 245))
+    # sc.blit(image,(0,0))
+    sc.blit(image3,rect)
+    pygame.display.flip()
